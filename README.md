44750d131245b728f0e9176d503371029d29dace
brozish <brozish@gmail.com>
2018-12-28 13:53:42 +0300

    add scheduling task with systemd timer for update docker containers and README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Docker CI/CD #
+
+CoreOS with nginx, gitlab ce, certbot and gitlab runner docker containers
+
+## License ##
+
+MIT
--- scheduling/scheduling.sh
+++ scheduling/scheduling.sh
@@ -2,17 +2,18 @@ su -
 
 vim /etc/systemd/system/certbot-renew.service
 vim /etc/systemd/system/certbot-renew.timer
-systemctl enable certbot-renew.timer
-systemctl enable certbot-renew
+systemctl enable --now certbot-renew.timer
 systemctl start certbot-renew.timer
-systemctl start certbot-renew
 
 vim /etc/systemd/system/backup-gitlab.service
 vim /etc/systemd/system/backup-gitlab.timer
-systemctl enable backup-gitlab.timer
-systemctl enable backup-gitlab
+systemctl enable --now backup-gitlab.timer
 systemctl start backup-gitlab.timer
-systemctl start backup-gitlab
+
+vim update-docker-containers.timer
+vim update-docker-containers.service
+systemctl enable --now update-docker-containers.timer
+systemctl start update-docker-containers.timer
 
 systemctl list-timers --all
 
